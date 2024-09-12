d97ff29a6b4d3be215bf967125fa0f680b0a4ad6
DraCaligari <tatianagarcia0695@hotmail.com>
2024-09-12 10:18:39 -0600

    Initial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HeroSection from "@/components/HeroSection.vue";
-import SkillSection from "@/components/SkillSection.vue";
-import ProjectSection from '@/components/ProjectSection.vue'
+import HeroSection from "../components/HeroSection.vue";
+import SkillSection from "../components/SkillSection.vue";
+import ProjectSection from '../components/ProjectSection.vue'
 import ContactMeSection from '@/components/ContactMeSection.vue'
 </script>
 
